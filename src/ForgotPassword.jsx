21dfa6f95c5dede467fb6543cd6da7f23a476483
simonkadili47 <simonkadili47@gmail.com>
2024-11-17 11:34:05 +0300

    forgot password

--- src/ForgotPassword.jsx
+++ src/ForgotPassword.jsx
@@ -1,20 +1,30 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import LOGO from './assets/logo.png';
-import Button from './components/buttons/Button'
+import Button from './components/buttons/Button';
 
 const ForgotPassword = () => {
   return (
-    <div className="min-h-screen flex items-start font-poppins justify-center bg-white"> 
+    <div className="min-h-screen flex items-start font-poppins justify-start bg-white"> 
       <div className="w-full max-w-md mt-4"> 
         <div className="flex justify-center">
           <img src={LOGO} alt="Logo" className="h-16" />
         </div>
-        <h2 className='text-3xl font-bold text-black'>Welcome Back to Amecore!</h2>
 
-        <div className="bg-gray-100 p-6 rounded-lg shadow-md mt-2"> 
+        <h2 className="text-lg mt-2 text-left">
+          <Link to="/login" className="text-blue-600 underline hover:text-blue-800">
+            Go back to Sign in
+          </Link>
+        </h2>
+
+        <h2 className="text-3xl font-bold text-black text-left mt-4">
+          Forgot Password!
+        </h2>
+        <h2 className='text-md text-black text-left mt-4'>Enter your phone number to receive a verification code.</h2>
+
+        <div className="bg-gray-100 p-6 rounded-lg shadow-md mt-4"> 
           <form>
-            <div className="mb-2">
+            <div className="mb-4">
               <label className="block text-black text-md font-bold mb-2 text-left" htmlFor="email">
                 Email Address
               </label>
@@ -26,8 +36,6 @@ const ForgotPassword = () => {
               />
             </div>
 
-            
-
             <div className="flex items-center justify-between mb-4">
               <Button
                 intent="primary"
@@ -35,16 +43,10 @@ const ForgotPassword = () => {
                 className="w-full" 
                 type="submit"
               >
-                Sign In
+                Send verification code
               </Button>
             </div>
 
-            <div className="flex justify-center text-center mt-4">
-              <p className="text-gray-700 text-sm sm:text-base inline">Don't have an account?</p>
-              <Link to="/register" className="text-black font-bold text-sm sm:text-base hover:underline inline ml-1">
-                Sign up
-              </Link>
-            </div>
           </form>
         </div>
       </div>
